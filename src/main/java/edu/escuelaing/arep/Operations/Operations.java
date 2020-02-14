ea617d7a5fbce0782e51ae8195e0335ddc38f671
nontoa <nicolasnc010@gmail.com>
2020-02-14 11:04:23 -0500

    primer commit 4

--- src/main/java/edu/escuelaing/arep/Operations/Operations.java
+++ src/main/java/edu/escuelaing/arep/Operations/Operations.java
@@ -1,11 +1,18 @@
 package edu.escuelaing.arep.Operations;
 
-
-
+import java.util.ArrayList;
 
 public class Operations{
 
+    public static float Sumatoria(ArrayList<Float> lista){
+        float resultado = 0;
+        for(float a : lista){
+            resultado += a;
+        }
+
+        return resultado;
+
+    }
 
 
-    
 }
