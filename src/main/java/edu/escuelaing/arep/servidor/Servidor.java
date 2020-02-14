5b839503b26ce92c36ee2a6fac92ee5d08b5106d
nontoa <nicolasnc010@gmail.com>
2020-02-14 11:31:20 -0500

    operaciones

--- src/main/java/edu/escuelaing/arep/servidor/Servidor.java
+++ src/main/java/edu/escuelaing/arep/servidor/Servidor.java
@@ -2,8 +2,13 @@
 package edu.escuelaing.arep.servidor;
 
 import static spark.Spark.*;
+
+import java.util.ArrayList;
+
 import com.google.gson.Gson;
 import edu.escuelaing.arep.Operations.*;
+import spark.Request;
+import spark.Response;
 
 
 
@@ -12,7 +17,8 @@ public class Servidor {
   public Servidor() {
     port(getPort());
     get("/hello", (req, res) -> "Hello Heroku");
-    //get("/numeros", (req, res) -> PaginaInicial(req, res));
+    get("/numeros", (req, res) -> PaginaInicial(req, res));
+    get("/resultado", (req, res) -> PaginaResultado(req, res));
    /**port(getPort());
    staticFiles.location("/pages");
    Gson json = new Gson();
@@ -25,6 +31,39 @@ public class Servidor {
    });*/
 
  }
+
+
+public String PaginaInicial(Request rq, Response rs){
+  String pagina = "<!DOCTYPE html>"
+  + "<html>"
+  + "<body>"
+  + "<h1>PARCIAL 1</h1>"
+  + "<h2>AREP 2020-1</h2>"
+  + "<h3>Ingrese los datos:</h3>"
+  + "<form action=\"/results\">"
+  + "  Ingrese los números separados por espacio:<br>"
+  + "  <input type=\"text\" name=\"num\" value=\"\">"
+  + "  <br>"
+  + "  <br> <br>"                  
+  + "  <input type=\"submit\" value=\"Enviar\">"
+  + "</form>"
+  + "</body>"
+  + "</html>";
+  return pagina;
+}
+
+public static String PaginaResultado(Request req, Response res) {        
+  String[] lista=req.queryParams("num").split("\\s+");    
+  ArrayList<Float> listaNumeros = new ArrayList<Float>();    
+  for(int a=0;a<lista.length;a++){
+      listaNumeros.add(Float.parseFloat(lista[a]));
+  }    
+  String resultado = "{\"lista\":"+"\""+listaNumeros+"\""+","+",\"sumatoria\":"+"\""+Operations.Sumatoria(listaNumeros)+"}";       
+  return resultado;
+} 
+
+
+ 
  
  static int getPort() {
    if (System.getenv("PORT") != null) {
